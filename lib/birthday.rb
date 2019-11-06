ba43ec284712c4ffed4f3b32069c23b93daad361
Henry Allan <henryallan50@gmail.com>
2019-11-06 20:51:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,7 +8,7 @@ def happy_birthday(birthday_kids)
   
   birthday_kids.each do |kids_name, age|
     
-    puts "happy Birthday #{kids_name}! You are now #{age} years old!"
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     
   end
   
